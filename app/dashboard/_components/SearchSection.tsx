b9d6355554c7ddd9ff438dc39798ab1b8ce9e1fd
Megdela <megdela21@gmail.com>
2025-04-02 03:11:57 +0300

    search functionality

--- app/dashboard/_components/SearchSection.tsx
+++ app/dashboard/_components/SearchSection.tsx
@@ -1,7 +1,7 @@
 import { Search } from 'lucide-react';
 import React from 'react'
 
-function SearchSection (){
+function SearchSection ({onSearchInput}:any){
   return (
     <div className="p-10 bg-gradient-to-br from -purple-500 via-purple-700 to-blue-600 flex flex-col justify-center items-center text-white">
       <h2 className="text-3xl font-bold">Browse All Templates</h2>
@@ -12,6 +12,7 @@ function SearchSection (){
           <input
             type="text"
             placeholder="Search"
+            onChange={(event)=>onSearchInput(event.target.value)}
             className="bg-transparent w-full outline-none text-black"
           />
         </div>
--- app/dashboard/_components/TemplateListSection.tsx
+++ app/dashboard/_components/TemplateListSection.tsx
@@ -1,5 +1,5 @@
 import Templates from '@/app/(data)/Templates'
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import TemplateCard from './TemplateCard';
 
 export interface TEMPLATE{
@@ -17,14 +17,24 @@ export interface FORM{
 name:String;
 required?:boolean
 }
-function TemplateListSection() {
+function TemplateListSection({ userSearchInput }:any) {
+    //search functionality
+    const[templateList,setTemplateList]=useState(Templates)
+    useEffect(()=>{
+if(userSearchInput){
+    const filterData=Templates.filter(item=>item.name.toLowerCase().includes(userSearchInput.toLowerCase()));
+    setTemplateList(filterData);
+} else{
+    setTemplateList(Templates)
+}
+    },[userSearchInput])
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-10'>
-      {Templates.map((item:TEMPLATE,index:number)=>(
-<TemplateCard {...item}/>
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-10">
+      {templateList.map((item: TEMPLATE, index: number) => (
+        <TemplateCard {...item} />
       ))}
     </div>
-  )
+  );
 }
 
 export default TemplateListSection
