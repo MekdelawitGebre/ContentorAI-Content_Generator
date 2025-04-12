f75afd0495a05649c36b2644200714bb269816a3
Megdela <megdela21@gmail.com>
2025-04-12 19:32:12 +0300

    unwrap promise for template slug

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -14,21 +14,16 @@ import moment from "moment";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 
-
-
-
 interface PROPS {
-  params: {
-    "template-slug": string;
-  };
+  params: Promise<{ "template-slug": string }>;
 }
 
-function CreateNewContent(props: PROPS) {
-  const params = props.params; 
+function CreateNewContent({ params }: PROPS) {
+  const actualParams = React.use(params); //unwrap the promise
   const selectedTemplate: TEMPLATE | undefined = Templates?.find(
-    
-    (item) => item.slug == params["template-slug"]
+    (item) => item.slug === actualParams["template-slug"]
   );
+
   const [loading, setLoading] = useState(false);
   const [aiOutput, setAiOutput] = useState<String>("");
 
@@ -38,51 +33,46 @@ function CreateNewContent(props: PROPS) {
     const FinalAIPrompt = JSON.stringify(formData) + ", " + SelectedPrompt;
 
     const result = await chatSession.sendMessage(FinalAIPrompt);
-    console.log(await result.response.text());
-    setAiOutput(result?.response.text());
-    await SaveInDb(formData, selectedTemplate?.slug, result?.response.text());
+    const outputText = await result.response.text();
+    setAiOutput(outputText);
+    await SaveInDb(formData, selectedTemplate?.slug, outputText);
     setLoading(false);
   };
 
   const { user } = useUser();
+
   const SaveInDb = async (formData: any, slug: any, aiResp: any) => {
-    const result = await db.insert(AIOutput).values({
-      formData: formData,
+    await db.insert(AIOutput).values({
+      formData,
       templateSlug: slug,
       aiResponse: aiResp,
       createdBy: user?.primaryEmailAddress?.emailAddress,
       createdAt: moment().format("DD/MM/yyyy hh:mm:ss"),
     });
   };
+
   const router = useRouter();
+
   return (
     <div className="p-4">
       {/* Back Button */}
       <Link href="/dashboard">
         <Button
-          onClick={() => {
-            console.log("Back button clicked");
-            router.push("/dashboard");
-          }}
+          onClick={() => router.push("/dashboard")}
           className="flex items-center gap-2 hover:cursor-pointer"
         >
           <ArrowLeft className="mr-2 h-4 w-4" />
           Back
         </Button>
       </Link>
-      
 
       {/* Content Sections */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 py-5">
-        {/* Left Side: FormSection */}
         <FormSection
           selectedTemplate={selectedTemplate}
-          userFormInput={(v: any) => {
-            GenerateAIContent(v);
-          }}
+          userFormInput={(v: any) => GenerateAIContent(v)}
           loading={loading}
         />
-
         <div className="col-span-2">
           <OutputSection aiOutput={aiOutput} />
         </div>
